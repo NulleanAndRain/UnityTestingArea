1616ce4b2baa2f72d3019e9d6093efaefd5b7f85
Nullean <mr000lean@gmail.com>
2021-05-07 22:29:47 +0400

    Init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.W)) {
+            rb.AddForce(Vector3.forward * Time.deltaTime * speed, ForceMode.VelocityChange);
+		}
+
+        if (Input.GetKey(KeyCode.S)) {
+            rb.AddForce(Vector3.back * Time.deltaTime * speed, ForceMode.VelocityChange);
+        }
+    }
+}
